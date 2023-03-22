a2617e89acfff2a3aeb06c9f6e5aabf210d5c7cb
Se Jin Park <zcabsjp@ucl.ac.uk>
2023-03-22 04:45:50 +0000

    Update requirements.txt

---
+++ submit_flow_jobs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Parse command line arguments
+while getopts "t:e:" opt; do
+  case $opt in
+    t) target="$OPTARG" ;;
+    e) epochs="$OPTARG" ;;
+    *) exit 1 ;;
+  esac
+done
+
+# Check if name and age are provided
+if [[ -z "$target" || -z "$epochs" ]]; then
+  echo "Error: Both corpus name (-t) and epochs (-e) are required." >&2
+  exit 1
+fi
+
+job_name="${target}_NER_Training_with_${epochs}_Epochs"
+
+# Print the values of the arguments to the console
+qsub -N ${job_name} /home/sejipark/NLP-FYP-HPC/ner/train_ner.qsub.sh -v arg1=target arg2=epochs
