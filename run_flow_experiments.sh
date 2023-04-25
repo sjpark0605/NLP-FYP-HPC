94911e7cc4db4b31a44ac25021bb3efc545e6165
Se Jin Park <zcabsjp@ucl.ac.uk>
2023-04-25 09:03:31 +0100

    Add Experimentation Script

---
+++ run_flow_experiments.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+arguments1=("r-100" "r-200" "r-300")
+arguments2=(0.0 0.5 0.25 0.1)
+
+for arg1 in "${arguments1[@]}"
+do
+  for arg2 in "${arguments2[@]}"
+  do
+    ./submit_flow_jobs.sh -t "$arg1" -u "$arg2" -e 6
+    # ./submit_flow_jobs.sh -t "$arg1" -u "$arg2" -e 6 --weighted
+  done
+done
