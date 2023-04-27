40c95fd30f51d9bc4cda9b3d07f1a1b511c52b83
Se Jin Park <zcabsjp@ucl.ac.uk>
2023-04-27 11:20:02 +0100

    Switch Run Order

--- run_flow_experiments.sh
+++ run_flow_experiments.sh
@@ -4,7 +4,7 @@ mkdir -p logs/unweighted
 mkdir -p logs/weighted
 
 arguments1=("r-300" "r-200" "r-100")
-arguments2=(0.0 0.5 0.25 0.9)
+arguments2=(0.9 0.5 0.25 0.0)
 
 for arg1 in "${arguments1[@]}"
 do
