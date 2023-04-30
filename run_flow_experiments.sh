06ada7eba54084b5701900e4742561913248f4d4
Se Jin Park <zcabsjp@ucl.ac.uk>
2023-05-01 00:11:30 +0100

    Replace 6 epochs with 4 epochs

--- run_flow_experiments.sh
+++ run_flow_experiments.sh
@@ -10,7 +10,7 @@ for arg1 in "${arguments1[@]}"
 do
   for arg2 in "${arguments2[@]}"
   do
-    ./submit_flow_jobs.sh -t "$arg1" -u "$arg2" -e 6
-    # ./submit_flow_jobs.sh -t "$arg1" -u "$arg2" -e 6 --weighted
+    ./submit_flow_jobs.sh -t "$arg1" -u "$arg2" -e 4
+    # ./submit_flow_jobs.sh -t "$arg1" -u "$arg2" -e 4 --weighted
   done
 done
