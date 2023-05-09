c7b0fd07a5e705e74b24abe35e453f74d4b8a454
Se Jin Park <zcabsjp@ucl.ac.uk>
2023-05-09 17:14:17 +0100

    Set Batch Size to 8

--- run_flow_experiments.sh
+++ run_flow_experiments.sh
@@ -3,13 +3,10 @@
 mkdir -p logs/unweighted
 mkdir -p logs/weighted
 
-arguments1=("r-300" "r-200" "r-100")
 arguments2=(0.9 0.75 0.5 0.0)
 
-for arg1 in "${arguments1[@]}"
+for arg2 in "${arguments2[@]}"
 do
-  for arg2 in "${arguments2[@]}"
-  do
-    ./submit_weighted_flow_jobs.sh -t "$arg1" -u "$arg2" -e 6
-  done
+  ./submit_flow_jobs.sh -t "r-300" -u "$arg2" -e 6
+  ./submit_weighted_flow_jobs.sh -t "r-300" -u "$arg2" -e 6
 done
