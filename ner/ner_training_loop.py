287ad7d05c8279811ef6f69996127ba685b2008d
Se Jin Park <zcabsjp@ucl.ac.uk>
2023-05-10 17:20:24 +0100

    Increase Batch Size for NER to 128

--- ner/ner_training_loop.py
+++ ner/ner_training_loop.py
@@ -104,13 +104,13 @@ label2id = {v: k for k, v in id2label.items()}
 train_dataloader = DataLoader(
   tokenized_datasets["train"],
   collate_fn=data_collator,
-  batch_size=32,
+  batch_size=128,
 )
 
 eval_dataloader = DataLoader(
   tokenized_datasets["valid"], 
   collate_fn=data_collator, 
-  batch_size=32,
+  batch_size=128,
 )
 
 ner_model = AutoModelForTokenClassification.from_pretrained(
