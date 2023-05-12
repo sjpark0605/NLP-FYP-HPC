3cf985279736729ad953f9ac6f8e279095715668
Se Jin Park <zcabsjp@ucl.ac.uk>
2023-05-12 02:52:58 +0100

    Debug Graph

--- flow-graph-generation/generate_r-300_flow_graphs.py
+++ flow-graph-generation/generate_r-300_flow_graphs.py
@@ -454,6 +454,8 @@ for ner_file, flow_file in tqdm(
         os.makedirs(dest_folder)
 
     predicted_nodes, predicted_edges = food_recipe_to_predicted_flow_graph(ner_lines)
+    print(predicted_nodes)
+    print(predicted_edges)
     true_nodes, true_edges = food_recipe_to_true_flow_graph(ner_lines, flow_lines)
 
     with open(dest_folder + "predicted_flow_graph.pkl", "wb") as pred_f:
